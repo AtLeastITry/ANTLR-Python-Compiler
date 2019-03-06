79cd64c8a48fd2908c3494369902367385b2f4a9
Matt Hope <matt@atlascode.com>
2019-03-06 15:46:14 +0000

    recompiled to python

--- src/output/python/example-function.py
+++ src/output/python/example-function.py
@@ -1,12 +1,12 @@
 
 a = 10
+def test():
+	return something(a, a)
 def something(c, d):
 	a = 20
 	def innersomething(x, y):
 		return (x + 3) * y * 3
 	a = innersomething(c, d)
 	return a
-def test():
-	return something(a, a)
 a = something(1, 2)
 b = test()
