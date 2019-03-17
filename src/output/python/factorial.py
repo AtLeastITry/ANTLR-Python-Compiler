7262327ca19b36b0a98868c3de68657dd2f2f2d2
AtLeastITry <matthew.hope396@outlook.com>
2019-03-17 14:51:17 +0000

    Fixed sample code.

--- src/output/python/factorial.py
+++ src/output/python/factorial.py
@@ -1,7 +1,7 @@
 n = 8
 val = 1
 counter = 1
-print(counter)
-while counter <= 5:
-	val = val + counter
+while counter <= n:
+	val = val * counter
 	counter = counter + 1
+print(val)
--- src/output/python/fibonacci.py
+++ src/output/python/fibonacci.py
@@ -3,6 +3,7 @@ previous = 0
 current = 1
 index = 1
 while index <= n:
+	print(current)
 	temp = current
 	current = current + previous
 	previous = temp
